b62836e1c27f394837fa7820a76e6b5df0e90515
Stephen Ramsay <rabrandom@gmail.com>
2019-11-21 15:23:27 +0000

    started activity 4

--- friends.rb
+++ friends.rb
@@ -1,3 +1,21 @@
+# 1.
+
 def get_name(person)
   return person[:name]
 end
+
+# 2.
+def get_favourite_tv_show(person)
+  return person[:favourites][:tv_show]
+end
+
+# 3.
+def check_favourite_food(person, food)
+  if person[:favourites][:snacks] = food
+    return true
+  else
+    false
+  end
+end
+
+# 4.
